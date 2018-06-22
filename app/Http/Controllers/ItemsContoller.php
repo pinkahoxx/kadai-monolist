65dd6aa96990ebdfa18b5286eb2b27304b92c1cf
kaho.yoshii <ec2-user@ip-172-31-27-95.us-east-2.compute.internal>
2018-06-22 04:40:19 +0000

    want

--- app/Http/Controllers/ItemsContoller.php
+++ app/Http/Controllers/ItemsContoller.php
@@ -36,6 +36,18 @@ use \App\Item;
         return view('items.create', [
             'keyword' => $keyword,
             'items' => $items,
-        ]);
+            ]);
     }
-  }
+
+        public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
+
+    }
